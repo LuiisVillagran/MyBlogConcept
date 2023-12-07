f89372d0724e5910f55d618889cef7bcd5795f7d
LuiisVillagran <luis.e.villa@hotmail.com>
2023-12-07 12:27:45 -0600

    MyBlogCommit

---
+++ js/jsblog.js
@@ -0,0 +1,27 @@
+
+/*----------------------LLAMAR A NAVBAR-----------------------*/
+fetch('../pages/nav.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar').innerHTML = data;
+    });
+
+/*----------------------LLAMAR A FOOTER-----------------------*/
+fetch('../pages/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
+
+$(document).ready(function(){
+    var navbar = $('#navbar');
+    var sticky = navbar.offset().top;
+
+    $(window).scroll(function(){
+        if (window.pageYOffset >= sticky){
+            navbar.addClass("sticky")
+        } else {
+            navbar.removeClass("sticky");
+        }
+    });
+});
